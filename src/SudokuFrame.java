f0a8d1e4b29bb78a93803c0f93305201bcce55ee
djkris27 <djkrzych@o2.pl>
2022-03-13 20:58:23 +0100

    creating GUI

--- src/SudokuFrame.java
+++ src/SudokuFrame.java
@@ -1,3 +1,14 @@
+/*
+Elementy, które należy umieścić:
+- obszar tekstowy do wpisywania sudoku
+- obszar tekstowy do wpisywania rozwiązanego sudoku
+- przycisk do wywołania rozwiązywania sudoku
+- przyciski przewijania rozwiązań (w przypadku więcej niż jednego rozwiązania
+- obszar tekstowy pokazujący ilość rozwiązań oraz aktualnie wyświetlane rozwiązanie (np. 1/3)
+- element obrazujący aktualną pracę (w przypadku długotrwałego rozwiązywania sudoku) oraz zakończenie rozwiązywania sudoku
+*/
+
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,10 +19,35 @@ public class SudokuFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 600);
         setLocation(50,50);
-        setLayout(new GridLayout());
+        setLayout(new FlowLayout());
 
-        JButton test = new JButton("Przycisk 1");
-        add(test);
+        JTextArea originSudoku = new JTextArea(
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 \n" +
+                " 0, 0, 0, 0, 0, 0, 0, 0, 0 "
+        );
+        JTextArea resolvedSudoku = new JTextArea(
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? \n" +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ? "
+        );
+        JButton resolve = new JButton("Rozwiąż sudoku");
+
+        add(originSudoku);
+        add(resolve);
+        add(resolvedSudoku);
 
         setVisible(true);
     }
